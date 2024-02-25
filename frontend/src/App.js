ada30c6989ed741c791c2592ceaf355322be2577
BNDPraveen <praveenbabi124@gmail.com>
2024-02-25 21:05:39 +0530

    Header & Footer components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,20 @@
 import React from "react";
+import Header from "./components/Header.js";
+import { Container } from "react-bootstrap";
+import Footer from "./components/Footer.js";
 
 const App = () => {
-  return <h1>Welcome To Potlings</h1>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <h1>Welcome to ProShop</h1>
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
