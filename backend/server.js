cdfe971d76c68e0c3dbecd1d304bb2d3c9fcf2b8
BNDPraveen <praveenbabi124@gmail.com>
2024-02-26 21:53:03 +0530

    Get products from database

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import connectDB from "./config/db.js";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 const port = process.env.PORT || 5050;
 
@@ -10,23 +10,12 @@ connectDB(); //connect to MongoDB
 
 const app = express();
 
+app.use("/api/products", productRoutes);
+
 app.get("/", (req, res) => {
   res.send("API is running");
 });
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((p) => p._id === req.params.id);
-  res.json(product);
-});
-
-app.get("/api/*", (req, res) => {
-  res.send("page not found");
-});
-
 app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${port}`);
 });
